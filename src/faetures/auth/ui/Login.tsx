5172ab4696db5bcbe4a6365a5db911bfd6b725bf
k2 <izmail.k2@gmail.com>
2023-12-27 17:19:56 +0300

    chore: add styles component Login

--- src/faetures/auth/ui/Login.tsx
+++ src/faetures/auth/ui/Login.tsx
@@ -1,7 +1,34 @@
+import {IconSvg} from "@/common/components/img/IconSvg.tsx";
+import s from './Login.module.scss'
 
 export const Login = () => {
   return (
-    <div>
+    <div className={s.container}>
+
+      <div className={s.blockLogo}>
+        <div className={s.logo}>
+          <IconSvg name={'logo'}/>
+          <h1>Talk Blink</h1>
+        </div>
+        <p>Adaptive app for chat</p>
+      </div>
+
+      <div className={s.blockItem}>
+        <h2>Welcome to Talk Blink!</h2>
+        <div>
+          <div>
+            <IconSvg name={"google"}/>
+            <span>Sign In with Google</span>
+          </div>
+          <div>
+            <IconSvg name={"facebook"}/>
+            <span>Sign In with Facebook</span>
+          </div>
+        </div>
+      </div>
+
+      <div className={s.background2}>
+      </div>
 
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import {BrowserRouter, createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {Login} from "@/faetures/auth/ui/Login.tsx";
+import '@/assets/styles/index.scss'
 
 
 const router = createBrowserRouter([
@@ -13,9 +13,7 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
     <React.StrictMode>
       <RouterProvider router={router}/>
     </React.StrictMode>,
-  </BrowserRouter>
 )
