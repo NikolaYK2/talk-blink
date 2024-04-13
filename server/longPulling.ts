2ea86f2b244bfd695c7f6dcf33495e8280f5bee9
k2 <izmail.k2@gmail.com>
2024-04-13 15:45:25 +0300

    feat(server): add file longPulling get-messages and new-messages routes

---
+++ server/longPulling.ts
@@ -0,0 +1,32 @@
+import cors from 'cors';
+import express from 'express';
+import * as events from "node:events";
+
+const PORT = 5000;
+
+//управление событиями
+const emitter = new events.EventEmitter();
+
+
+const app = express();
+
+app.use(cors());
+
+//Для получения сообщений
+app.get('get-messages', (req, res) => {
+  //Уведомление что пользователь смс отправил
+  emitter.once('newMessages', (message) => {
+    res.json(message);
+  })
+})
+//На отправку
+app.post('new-messages', (req, res) => {
+  //достаем сообщение из тела запроса
+  const message = req.body;
+  emitter.emit('newMessages', message);
+  res.status(200)
+})
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+})
