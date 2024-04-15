c3dd9f0d20c612322638d680f4ada7bed87da8c0
k2 <izmail.k2@gmail.com>
2024-04-15 16:34:05 +0300

    feat: add RRD

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from "@/App.tsx";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import '@/assets/styles/index.scss'
+import {PATH} from "@/common/constants/conts.ts";
+import App from "@/App.tsx";
+
+
+const router = createBrowserRouter([
+  {
+    path: PATH.ROUT_MAIN,
+    element: <App/>,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App/>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
