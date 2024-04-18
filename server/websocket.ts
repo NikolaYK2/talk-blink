823cb7ddc2b61820d77d574e2365ad19de63edf1
k2 <izmail.k2@gmail.com>
2024-04-18 20:51:22 +0300

    feat(server): Implement WebSocket server and message broadcasting
    - Created a WebSocket server that listens on port 5000.
    - Defined a MessageType for the structure of the messages.
    - Implemented a connection event handler that listens for incoming messages.
    - Parsed incoming messages and handled 'message' and 'connection' events.
    - Created a broadcastMessage function that sends a message to all connected clients."

---
+++ server/websocket.ts
@@ -0,0 +1,44 @@
+import ws from 'ws'
+
+const wss = new ws.Server({
+  port: 5000,
+}, () => console.log('Server started'));
+
+
+type MessageType = {
+  event: 'message' | 'connection',
+  message: string,
+  id: number,
+  data: string,
+  username: string
+}
+wss.on('connection', function connection(ws) {
+  ws.on('message', function (message: MessageType) {
+    message = JSON.parse(message.toString()) as MessageType;
+
+    switch (message.event) {
+      case 'message': {
+        broadcastMessage(message);
+        break
+      }
+      case 'connection': {
+        broadcastMessage(message);
+        break
+      }
+    }
+  })
+})
+
+function broadcastMessage(message: MessageType) {
+  wss.clients.forEach((client) => {
+    client.send(JSON.stringify(message))
+  })
+}
+
+// const message = {
+//   event: 'message/connection',
+//   id: 123,
+//   data: new Date().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'}),
+//   username: 'Nikolos',
+//   message: 'Hello World!'
+// }
