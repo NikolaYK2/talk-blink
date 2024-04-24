4a883c72645b08f638080105dfbc7b05611f91f9
k2 <izmail.k2@gmail.com>
2024-04-24 22:02:00 +0300

    style(Chats): upd style

--- client/src/features/userHub/ui/chats/Chats.tsx
+++ client/src/features/userHub/ui/chats/Chats.tsx
@@ -1,19 +1,22 @@
 import {useUserHub} from "@/features/userHub/lib/useUserHub.ts";
+import s from './Chats.module.scss'
+import {IconSvg} from "@/common/components/IconSVG.tsx";
 
 type Props = {
   title: string
 }
 export const Chats = ({title}: Props) => {
   const {users} = useUserHub();
-  console.log(users)
 
   return (
-    <section>
+    <section className={s.container}>
       <h2 className={'h2App'}>{title}</h2>
-      <div>
+      <div className={s.block}>
         {users.map(el =>
-          <div key={el.id}>
-            <div>{el.username}</div>
+          <div key={el.id} className={s.item}>
+            <div className={s.avatar}><IconSvg name={"ava"}/></div>
+            <div className={s.name}>{el.username}</div>
+            <div className={s.data}>{el.data}</div>
           </div>
         )}
       </div>
